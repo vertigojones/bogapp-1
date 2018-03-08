3df90de781d4c8945a9e812c3b0d4673cc4baa2d
owenliversidge <owen.liversidge@gmail.com>
2018-03-08 12:04:47 -0500

    port running

--- server.js
+++ server.js
@@ -6,3 +6,29 @@ const logger = require("morgan")
 
 const app = express()
 
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', err => {
+  console.log(err)
+})
+
+db.on('open', () => {
+    console.log('Connected to MongoDB')
+})
+
+// middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+// set up routes
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Bog of Eternal Stench is up and running on PORT 3001')
+})
